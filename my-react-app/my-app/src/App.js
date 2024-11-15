2fc000c47c6f6e36eaffa5aaf1a860e464768063
Bomi Do <hellobomiii@gmail.com>
2024-11-14 22:42:59 -0500

    2024/11/14

--- my-react-app/my-app/src/App.js
+++ my-react-app/my-app/src/App.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './LandingPage';
-import NextPage from './NextPage'; // This is the component for the next page.
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/next-page" element={<NextPage />} />
-      </Routes>
-    </Router>
-  );
+  const [youtubeLink, setYoutubeLink] = useState ('');
+  const [components, setComponents] = useState([]);
+
+  const handleInputChange = (e) => {
+    setYoutubeLink(e.target.value);
+  };
+
+  const handleButtonClick = () => {
+    setComponents([
+      'Intro',
+      'Verse 1',
+      'Chorus',
+      'Verse 2',
+      'Bridge',
+      'Chorus',
+      'Outro'
+    ]);
+  };
+
+  const extractVideoID = (url) => {
+    const regex = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/\s]{11})/;
+    const match = url.match(regex);
+    return match ? match[1] : null;
+  };
+
+  return(
+    <div className ="App">
+      <h1>Music Component Analyzer</h1>
+      <input
+        type="text"
+        placeholder="Drop the beat"
+        value={youtubeLink}
+        onChange={handleInputChange}
+    />
+    <button onClick={handleButtonClick}>Analyze Music</button>
+
+    {youtubeLink &&(
+      <div className="video-container">
+        <iframe
+          width="560"
+          height="315"
+          src={'https://www.youtube.com/embed/${extractVideoID(youtubeLink)}'}
+          title="Youtube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+    )}
+
+    {components.length > 0 && (
+      <div className="components-list">
+        <h2>Music yo</h2>
+        <ul>
+          {components.map((component, index) => (
+            <li key={index}>{component}</li>
+          ))}
+        </ul>
+      </div>
+    )}
+
+    </div>
+   );
 }
 
 export default App;
